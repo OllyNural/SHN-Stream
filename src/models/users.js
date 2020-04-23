87bced05ebe8bdefede642fe8772de13b6434925
Olly Nural <ollynural@ollys-mbp.home>
2020-04-23 14:48:01 +0100

    Massive rabbit hole of testing fs.readFile. Refactored the users get to use file system not js files... derp

--- src/models/users.js
+++ src/models/users.js
@@ -1,11 +1,23 @@
 const { userData } = require('../data/users.json')
+const fs = require('fs');
+const path = require('path')
 
-exports.getById = (id) => {
-    const user = userData.find((user) => user.id === id)
-    return user ? user : null;
-}
+// exports.getById = (id) => {
+//     const user = userData.find((user) => user.id === id)
+//     return user ? user : null;
+// }
 
-exports.getStreamsById = (id) => {
-    const user = userData.find((user) => user.id === id)
-    return user ? user.streams : null
+exports.getStreamsByIdJson = (id) => {
+    return new Promise((res, rej) => {
+        fs.readFile(path.resolve(__dirname, '../data/users.json'), 'utf8', (err, data) => {
+            if (err) {
+                rej(err)
+            }
+
+            const userData = JSON.parse(data)
+
+            const user = userData.find((user) => user.id === id)
+            res(user ? user.streams : [])
+        });
+    })
 }
--- src/models/users.test.js
+++ src/models/users.test.js
@@ -1,39 +1,56 @@
-jest.mock('../data/users.json', () => ({
-    userData: [
-        {
-            "id": 5,
-            "streams": [
-                "streamId",
-                "streamId"
-            ]
-        }
-    ]
-}))
+const userData = [
+    {
+        "id": 5,
+        "streams": [
+            "streamId",
+            "streamId"
+        ]
+    }
+]
+
+// const mock = require('mock-fs');
+
+// jest.mock('../data/users.json', () => ({
+//     userData: [
+//         {
+//             "id": 5,
+//             "streams": [
+//                 "streamId",
+//                 "streamId"
+//             ]
+//         }
+//     ]
+// }))
 const users = require('./users.js');
 
-describe('getById', () => {
-    it('Should return null for non-existent user', () => {
-        const user = users.getById(1);
-        expect(user).toBeNull();
-    });
+const fs = require('fs');
+jest.mock('fs')
 
-    it('Should return user for existing user', () => {
-        const user = users.getById(5);
-        expect(user.id).toBe(5);
-    });
+describe('getById', () => {
+    // it('Should return null for non-existent user', () => {
+    //     const user = users.getById(1);
+    //     expect(user).toBeNull();
+    // });
+
+    // it('Should return user for existing user', () => {
+    //     const user = users.getById(5);
+    //     expect(user.id).toBe(5);
+    // });
 });
 
 describe('getStreamsById', () => {
-    it('Should return empty array for non-existent user', () => {
-        const streams = users.getStreamsById(1);
-        expect(streams).toBeNull();
-    });
-    
-    // Never a case where user exists with an empty array
+    it('Should return empty array of streams for non-existing user', async (done) => {
+        fs.readFile = jest.fn().mockImplementation((file, option, cb) => cb(null, JSON.stringify(userData)))
+        const streams = await users.getStreamsByIdJson(1);
+        expect(streams.length).toBe(0);
+        done()
+    })
 
-    it('Should return streams for existing user', () => {
-        const streams = users.getStreamsById(5);
+    it('Should return correct array of streams for existing user', async (done) => {
+        fs.readFile = jest.fn().mockImplementation((file, option, cb) => cb(null, JSON.stringify(userData)))
+        const streams = await users.getStreamsByIdJson(5);
         expect(streams.length).toBe(2);
         expect(streams[0]).toBe("streamId");
-    });
+        done()
+    })
 });
