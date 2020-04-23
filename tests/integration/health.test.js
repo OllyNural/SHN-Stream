3d8f2191c0d60be7ac199b690067212dfd16e15b
Olly Nural <ollynural@ollys-mbp.home>
2020-04-23 13:32:10 +0100

    Added healthcheck endpoint test. Already faileda at TDD

---
+++ tests/integration/health.test.js
@@ -0,0 +1,12 @@
+const app = require('../../index.js');
+const supertest = require('supertest');
+const request = supertest(app)
+
+describe('Health Check', () => {
+    it('Should return a 200 for /health', async (done) => {
+        const res = await request.get('/health');
+        expect(res.status).toBe(200);
+        done()
+
+    })
+})
