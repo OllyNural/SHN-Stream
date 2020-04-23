25916161a8851022140e14f415da3ce5b796228d
Olly Nural <ollynural@ollys-mbp.home>
2020-04-23 13:50:09 +0100

    Added some basic logging, and wrote out ideas for video.js structure

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const { video } = require('./routes');
 
-app.use(express.json());
+const loggerFormat = require('./utils/logger')
+
+// Log errors to console
+app.use(morgan(loggerFormat, {
+    skip: function (req, res) { return res.statusCode < 400 }
+}))
+
+// Log normal access to a log file
+app.use(morgan(loggerFormat, {
+    stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+}))
 
 app.use('/video', video)
 
